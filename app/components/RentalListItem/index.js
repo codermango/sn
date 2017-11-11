affff5bd6e83fc3f67bc209a38953858a503fc4b
codermango <codermango@gmail.com>
2017-11-11 11:22:07 +0100

    Update

--- app/components/RentalListItem/index.js
+++ app/components/RentalListItem/index.js
@@ -19,14 +19,48 @@ const styles = {
   },
   price: {
     minWidth: '90px',
-    backgroundColor: '#aab0c6',
+    backgroundColor: '#4286f5',
     color: '#fff',
     fontWeight: '700'
+  },
+  title: {
+    marginBottom: '10px'
+  },
+  detail: {
+    display: 'flex'
+  },
+  detailItem: {
+    height: '20px',
+    margin: '0 5px',
+    fontSize: '10px'
   }
 };
 
 function RentalListItem(props) {
   const { data, classes } = props;
+  const title = data && data.title;
+  const detail = data && data.detail;
+
+  const address = detail && detail.address;
+  const size = detail && detail.size.trim() && `${detail.size}㎡`;
+  const roomNum = detail && detail.roomNum && `房间数：${detail.roomNum}`;
+  const period = detail && detail.period;
+  let periodText = '';
+  if (period && period.startDate) {
+    periodText = period.isLongTermRental ? `从${period.startDate}起长期` : `从${period.startDate}到${period.endDate}`;
+  }
+
+  const primaryText = title ? (
+    <div className={classes.title}>{title}</div>
+  ) : '';
+  const secondaryText = detail ? (
+    <div className={classes.detail}>
+      { address && <Chip style={{marginLeft: "0"}} className={classes.detailItem} label={address} /> }
+      { periodText && <Chip className={classes.detailItem} label={periodText} /> }
+      { size && <Chip className={classes.detailItem} label={size} /> }
+      { roomNum && <Chip className={classes.detailItem} label={roomNum} /> }
+    </div>
+  ) : '';
   return data ? (
     <ListItem className={classes.item} button>
       <ListItemAvatar>
@@ -35,8 +69,9 @@ function RentalListItem(props) {
         </Avatar>
       </ListItemAvatar>
       <ListItemText
-        primary={data.title}
-        secondary={data.detail.address}
+        primary={primaryText}
+        secondary={secondaryText}
+        disableTypography={true}
       />
       <Chip label={`${data.detail.price}kr`} className={classes.price} />
     </ListItem>
